c0f5847bc9efa672d74ef0bd656920124322274b
Ashik Mahmood <wd.ashik04@gmail.com>
2024-08-05 04:34:46 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-
-
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Dashboard from './components/NavBarAssets/Dashboard';
-import Sidebar from './components/Sidebar/Sidebar';
-import Header from './components/Header/Header';
-import Purchase from './components/NavBarAssets/Purchase';
-import Attendance from './components/NavBarAssets/Attendance';
-import Expenses from './components/NavBarAssets/Expenses';
-import Stock from './components/NavBarAssets/Stock';
-import TotalReport from './components/NavBarAssets/TotalReport';
-import Setting from './components/NavBarAssets/Setting';
-import UserAdmin from './components/NavBarAssets/UserAdmin';
-import Contacts from './components/NavBarAssets/Contacts';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Dashboard from "./components/NavBarAssets/Dashboard";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
+import Purchase from "./components/NavBarAssets/Purchase";
+import Attendance from "./components/NavBarAssets/Attendance";
+import Expenses from "./components/NavBarAssets/Expenses";
+import Stock from "./components/NavBarAssets/Stock";
+import TotalReport from "./components/NavBarAssets/TotalReport";
+import Setting from "./components/NavBarAssets/Setting";
+import UserAdmin from "./components/NavBarAssets/UserAdmin";
+import Contacts from "./components/NavBarAssets/Contacts";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -23,12 +27,15 @@ function App() {
 
   return (
     <Router>
-        
-      <div className="flex overflow-hidden">
+      <div className="flex overflow-hidden min-h-screen">
         <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className={`flex flex-col flex-1 transition-all duration-300 ${isSidebarOpen ? 'ml-52' : 'ml-16'}`}>
+        <div
+          className={`flex flex-col flex-1 transition-all duration-300 ${
+            isSidebarOpen ? "ml-52" : "ml-16"
+          }`}
+        >
           <Header toggleSidebar={toggleSidebar} />
-          <main className="p-4">
+          <main className="flex-1 p-4">
             <Routes>
               <Route path="/" element={<Navigate to="/dashboard" />} />
               <Route path="/dashboard" element={<Dashboard />} />
@@ -42,6 +49,9 @@ function App() {
               <Route path="/attendance" element={<Attendance />} />
             </Routes>
           </main>
+         
+          <Footer className="mt-auto" />
+         
         </div>
       </div>
     </Router>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="border px-4 py-6 bg-white">
+      <p className="">
+        <i>Copyright © 2024 All rights reserved.</i>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
